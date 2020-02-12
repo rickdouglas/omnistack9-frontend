e4a40756efa1823e9df433108b2e9a1d45cbe1ea
rickdouglas <rickmiranda23@gmail.com>
2020-02-11 21:27:30 -0300

    Começanco o front da aplicaçao

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import logo from './assets/logo.svg';
+
+function App() {
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCNC" />
+      
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
+        </p>
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input id="email" type="email" placeholder="Seu e-mail"></input>
+          
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
