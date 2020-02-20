92227828f5f167e7910d1cdac5b12efe0f1a8c3c
rickdouglas <rickmiranda23@gmail.com>
2020-02-19 22:52:24 -0300

    Tela de cadastro de Spots

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
+
 import './App.css';
 import logo from './assets/logo.svg';
+import Routes from './routes';
 
 function App() {
+
   return (
     <div className="container">
-      <img src={logo} alt="AirCNC" />
+      <img src={logo} alt="AirCnC" />
       
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
-        </p>
-        <form>
-          <label htmlFor="email">E-MAIL *</label>
-          <input id="email" type="email" placeholder="Seu e-mail"></input>
-
-          <button className="btn" type="submit">Entrar</button>
-          
-        </form>
+        <Routes />
       </div>
     </div>
   );
